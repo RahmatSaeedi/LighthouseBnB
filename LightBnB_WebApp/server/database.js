fe00b3ee60ba85b1e5362b6e75fea1da4b5d831c
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-29 21:16:18 -0600

    connected past reservation system to the DB

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -65,7 +65,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guestId, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT 
+      properties.*,
+      reservations.*,
+      avg(property_reviews.rating) AS average_rating
+    FROM reservations
+    LEFT JOIN properties ON properties.id = reservations.property_id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `,[guestId, limit]).then(resp => resp.rows);
 };
 exports.getAllReservations = getAllReservations;
 
